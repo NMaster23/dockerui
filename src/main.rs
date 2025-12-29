f70e61b37a28e939db8461d554dc788bf5064532
NMaster23 <122946076+NMaster23@users.noreply.github.com>
2025-12-28 19:41:14 -0800

    Initial Commit
    A commit of all the currently developed code.

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use std::fs;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+use std::io::Write;
+use serde::{Serialize, Deserialize};
+use std::net::TcpStream;
+use ssh2::Session;
+
+slint::include_modules!();
+
+#[derive(Serialize, Deserialize)]
+struct DockerCred {
+    username: String,
+    password: String,
+    ip_addr: String,
+}
+
+fn dockercreds(username_input: String, password_input: String, ip_addr_input: String) {
+    let dockercred = DockerCred {
+        username: username_input,
+        password: password_input,
+        ip_addr: ip_addr_input,
+    };
+    let auth = serde_json::to_string(&dockercred).unwrap();
+    let mut authentication = File::create("dockercred.crd").unwrap();
+    authentication.write_all(auth.as_bytes()).unwrap();
+}
+
+fn docker_commands(ip_addr_input: String, command: String, ui: &AppWindow) {
+    let connectionaddress = format!("{}:22", ip_addr_input);
+    let tcp = TcpStream::connect(connectionaddress).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    let dockerauth: DockerCred = serde_json::from_str(&fs::read_to_string("dockercred.crd").expect("Unable to read file")).unwrap();
+    sess.userauth_password(&dockerauth.username, &dockerauth.password).unwrap();
+    assert!(sess.authenticated());
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec(&command).unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("{}", channel.exit_status().unwrap());
+    let condition = channel.exit_status().unwrap();
+    let output = s;
+    if condition != 0 {
+        println!("Error 0. Unknown Error occurred.");
+    }
+    let _ = channel.close();
+    println!("Intended Disconnection Suceeded");
+    ui.set_commandoutput(output.into());
+}
+
+fn main() {
+    let ui = AppWindow::new().unwrap();
+    ui.on_send_credentials(|username_input: slint::SharedString, password_input: slint::SharedString, ip_addr_input: slint::SharedString| {
+        let uiusername = username_input.to_string();
+        let uipassword = password_input.to_string();
+        let uiipaddress = ip_addr_input.to_string();
+        dockercreds(uiusername, uipassword, uiipaddress);
+    });
+    if Path::new("D:\\VSCode\\hackclub\\dockerui\\dockercred.crd").exists() == true {
+        ui.set_credentialcreation(false);
+        let dockercred: DockerCred = serde_json::from_str(&fs::read_to_string("dockercred.crd").expect("Unable to read file")).unwrap();
+        let dockerip = dockercred.ip_addr.to_string();
+        docker_commands(dockerip, r"docker image list --format 'table {{.ID}}\t{{.Repository}}\t{{.Tag}}\t{{.Size}}'".to_string(), &ui);
+    }
+    ui.run().unwrap();
+}
